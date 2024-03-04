28bfc6543fb9cdd8f99a92f3a399685f16789943
munhoz2k <lucasmunhozarruda@gmail.com>
2024-03-04 19:11:15 -0300

    :paintbrush: Minor Components Styles Changes

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,14 +9,15 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
+        default: 'bg-primary text-primary-foreground hover:bg-primary/70',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
           'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+          'bg-secondary text-secondary-foreground dark:hover:bg-neutral-600 hover:bg-neutral-300',
+        ghost:
+          'hover:bg-neutral-300 dark:hover:bg-neutral-600 hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
