2da813a0f8c1310745d6fdc491e4cb7e37021164
munhoz2k <lucasmunhozarruda@gmail.com>
2024-03-06 15:22:31 -0300

    Including Date Range

--- src/api/get-daily-revenue-in-period.ts
+++ src/api/get-daily-revenue-in-period.ts
@@ -1,13 +1,27 @@
 import { api } from '@/lib/axios'
 
+export interface GetDailyRevenueInPeriodQuery {
+  from?: Date
+  to?: Date
+}
+
 export type GetDailyRevenueInPeriodRes = {
   date: number
   receipt: number
 }[]
 
-export async function getDailyRevenueInPeriod() {
+export async function getDailyRevenueInPeriod({
+  from,
+  to,
+}: GetDailyRevenueInPeriodQuery) {
   const { data } = await api.get<GetDailyRevenueInPeriodRes>(
     '/metrics/daily-receipt-in-period',
+    {
+      params: {
+        from,
+        to,
+      },
+    },
   )
 
   return data
