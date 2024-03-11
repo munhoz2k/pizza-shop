0b70d93f7c693ea1110542d662562fd74e794157
munhoz2k <lucasmunhozarruda@gmail.com>
2024-03-11 17:07:29 -0300

    Removing Comments

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import path from 'node:path'
+
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
