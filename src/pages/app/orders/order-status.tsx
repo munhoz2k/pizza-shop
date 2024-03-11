8a67620c41d4e0e8358fbb0069823dbf3879fb4a
munhoz2k <lucasmunhozarruda@gmail.com>
2024-03-11 16:59:27 -0300

    Structural Changes

--- src/pages/app/orders/order-status.tsx
+++ src/pages/app/orders/order-status.tsx
@@ -1,3 +1,5 @@
+import { HTMLProps } from 'react'
+
 export type OrderStatus =
   | 'pending'
   | 'processing'
@@ -9,7 +11,7 @@ interface OrderStatusProps {
   status: OrderStatus
 }
 
-const orderStatusMap: Record<OrderStatus, string> = {
+export const orderStatusMap: Record<OrderStatus, string> = {
   pending: 'Pendente',
   delivered: 'Entregue',
   delivering: 'Em entrega',
@@ -17,24 +19,28 @@ const orderStatusMap: Record<OrderStatus, string> = {
   canceled: 'Cancelado',
 }
 
+export const badgeColorMap: Record<OrderStatus, string> = {
+  pending: 'bg-slate-500',
+  processing: 'bg-amber-500',
+  delivering: 'bg-amber-500',
+  delivered: 'bg-emerald-500',
+  canceled: 'bg-rose-500',
+} as const
+
 export function OrderStatus({ status }: OrderStatusProps) {
+  const Badge = ({ className, ...props }: HTMLProps<HTMLSpanElement>) => {
+    return (
+      <span
+        data-testid="badge"
+        className={`size-2 rounded-full ` + className}
+        {...props}
+      />
+    )
+  }
+
   return (
     <div className="flex items-center gap-2">
-      {status === 'pending' && (
-        <span className="size-2 rounded-full bg-slate-400" />
-      )}
-
-      {['processing', 'delivering'].includes(status) && (
-        <span className="size-2 rounded-full bg-amber-500" />
-      )}
-
-      {status === 'delivered' && (
-        <span className="size-2 rounded-full bg-emerald-500" />
-      )}
-
-      {status === 'canceled' && (
-        <span className="size-2 rounded-full bg-rose-500" />
-      )}
+      <Badge className={badgeColorMap[status]} />
 
       <span className="font-medium text-muted-foreground">
         {orderStatusMap[status]}
